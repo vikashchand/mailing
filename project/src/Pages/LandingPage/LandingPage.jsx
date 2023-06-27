89f483042fc302129b5d3fde2f430a159faa9eea
vikash chand <vikash.chand2020@vitstudent.ac.in>
2023-06-27 10:41:36 +0530

    home page modified

--- project/src/Pages/LandingPage/LandingPage.jsx
+++ project/src/Pages/LandingPage/LandingPage.jsx
@@ -1,33 +1,54 @@
-// LandingPage.js
-
 import React from 'react';
+import { FaEnvelope, FaTasks, FaClock, FaChartLine, FaInstagram, FaGithub } from 'react-icons/fa';
+import { MdEmail, MdPhone } from 'react-icons/md';
 import './LandingPage.css';
-import emailImage from '../../assets/email.jpg'
-import featureImage from '../../assets/features.jpg'
+import emailImage from '../../assets/email.jpg';
+import featureImage from '../../assets/features.jpg';
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
      
-        <div className="feature-container">
-          <div className="feature-description">
-            <h2>Features</h2>
-            <ul>
-              <li>Create and customize email templates</li>
-              <li>Manage subscriber lists and segments</li>
-              <li>Schedule and send automated campaigns</li>
-              <li>Track and analyze campaign performance</li>
-            </ul>
-          </div>
-          <div className="feature-image">
-            <img src={emailImage} alt="Feature" />
-          </div>
+     
+     <br></br>
+     <br></br>
+     <br></br>
+        <h1>Email Marketing Management System</h1>
+        <p>Create, manage, and analyze your email campaigns</p>
+     
+
+      <div className="feature-container">
+        <div className="feature-description">
+          <h2>Features</h2>
+          <ul>
+            <li><FaEnvelope /> Create and customize email templates</li>
+            <li><FaTasks /> Manage subscriber lists and segments</li>
+            <li><FaClock /> Schedule and send automated campaigns</li>
+            <li><FaChartLine /> Track and analyze campaign performance</li>
+          </ul>
         </div>
-     
-     
+        <div className="feature-image">
+          <img src={emailImage} alt="Feature" />
+        </div>
+      </div>
+
+      <div className="contact-container">
+      <div className="contact-description">
+      <h2>Contact Us</h2>
+        <ul >
+       
+          <li><MdEmail /> email@example.com</li>
+          <li><MdPhone /> +1 123-456-7890</li>
+          <li><FaInstagram /> <a href="https://www.instagram.com/example/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+          <li><FaGithub /> <a href="https://github.com/example" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+        </ul>
+        </div>
+        <div className="feature-image">
         
-        <p>&copy; 2023 Email Marketing Management System. All rights reserved.</p>
-     
+      </div>
+    </div>
+
+      <p>&copy; 2023 Email Marketing Management System. All rights reserved.</p>
     </div>
   );
 }
